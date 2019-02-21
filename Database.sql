7a67e4e54f8e9d44e7b2158aab9ab0c080153fc2
Anantha Raju C <anantha120@gmail.com>
2019-02-21 08:08:32 +0530

    ADD MySQL FLUSH Commands

--- Database.sql
+++ Database.sql
@@ -42,6 +42,20 @@ grant all privileges on *.* to 'USERNAME'@'IPADDRESS' with grant option;
 
 ****************************************************************************************
 
+-- when we grant some privileges for a user, running the command flush privileges will reloads the grant tables in the mysql database enabling the changes to take effect without reloading or restarting mysql service.
+FLUSH PRIVILEGES;
+
+-- The command closes all tables which are currently open or in use. And clears cache which intern make better utilization on available memory.
+FLUSH TABLES;
+
+-- The command uses host cache tables, if maximum number of connections has been reached for a particular host, mysql server will not able to make new connections. flushing host tables resets the process and again allows the connections for particular HOST.
+FLUSH HOSTS;
+
+-- The command closes and reopens all log files, if log files are to big and taking more time to load then you can run the command which will create an empty log file.
+FLUSH LOGS;
+
+****************************************************************************************
+
 -- Dumping Data Using INSERT statement
 
 SET AUTOCOMMIT=O;
